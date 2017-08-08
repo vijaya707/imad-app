dc7df9540ada2a8cc8a4160879af0d8812c8a65b
vijaya707 <707viee@gmail.com>
2017-08-08 20:09:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/vjaxpage1', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','/ui/vjaxpage1.html'));
+  res.sendFile(path.join(__dirname,'ui','vjaxpage1.html'));
 });
 
 app.get('/vjaxpage2', function (req, res) {
