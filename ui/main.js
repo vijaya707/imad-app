ffb01bf2bd4f1f330518d87643c18c17b932fb21
vijaya707 <707viee@gmail.com>
2017-08-09 00:01:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,13 @@ var element = document.getElementById('text-content');
 element.innerHTML ='Made Some Local Changes';
 
 var imgelement = document.getElementById('img-id');
-
+var marginLeft = 10;
+function moveRight()
+{
+    marginLeft = marginLeft+10;
+    imgelement.style.marginLeft = marginLeft+ 'px';
+}
 imgelement.onclick = function() {
-    var interval = setInterval(imgelement.style.marginLeft = marginLeft+10 + 'px',100);
+    var interval = setInterval(moveRight,100);
    
 };
