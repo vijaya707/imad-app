96d3cf7bb93ac382cfeaab091069aeedd8861a65
vijaya707 <707viee@gmail.com>
2017-08-08 22:20:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content= {
+  title : 'Vjax Page 1 Article',
+  heading : 'Watch Dogs 2',
+  date : '8th August, 2017',
+  content :'<p>Watch Dogs 2  is an action-adventure video game developed by Ubisoft Montreal and published by Ubisoft. It is the sequel to 2014 Watch Dogs and was released worldwide for PlayStation 4, Xbox One and Microsoft Windows in November 2016.</p>'
+};
+
 app.get('/vjaxpage1', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','vjaxpage1.html'));
 });
