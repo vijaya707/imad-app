061b870a1c2ef48249281d88be1d839c21206c66
vijaya707 <707viee@gmail.com>
2017-08-08 23:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,9 +77,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/main.js',function(req,res){
-  res.sednFile(path.join(__dirname,'ui','main.js'));  
-});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -88,6 +85,9 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
