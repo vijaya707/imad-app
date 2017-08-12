c935051ccad11aeb6eb02b117ae1438a22b26313
vijaya707 <707viee@gmail.com>
2017-08-12 18:16:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var pool = new Pool(config);
 app.get('/test-db',function(req,res){
   pool.query('select * from user',function(err,result){
       if(err) {
-        res.status('Cannot fetch data'); 
+        res.status(500).send(err.toString()); 
       } else{
         res.send(JSON.stringify(result));
         }
