3db8641efbfc7e452c20fed21b4f068fb31f80fe
vijaya707 <707viee@gmail.com>
2017-08-12 18:12:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,26 +2,24 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+
 var config = {
     user : 'u707viee',
     database : 'u707viee',
     host : 'http://db.imad.hasura-app.io',
     port: '5432',
     password : process.env.DB_PASSWORD,
-};
+}
 
 var app = express();
 app.use(morgan('combined'));
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-  pool.query('Select * from user',function(err,result){
+  pool.query('select * from user',function(err,result){
       if(err) {
         res.status('Cannot fetch data'); 
-          
-      }
-        
-        else{
+      } else{
         res.send(JSON.stringify(result));
         }
   });
