3566f6255f708664d1397ab21a51682aeb5e9f40
vijaya707 <707viee@gmail.com>
2017-08-18 16:44:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article_one.html',function (req,res){
+app.get('/article_one',function (req,res){
    res.sendFile(path.join(__dirname,'ui','article_one.html')) ;
 });
 app.get('/ui/style.css', function (req, res) {
