321ee9ff8bbd5a63024d947f48348d9399214a22
vijaya707 <707viee@gmail.com>
2017-08-18 16:44:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-
-var config = {
-    user : 'u707viee',
-    database : 'u707viee',
-    host : 'db.imad.hasura-app.io',
-    port: '5432',
-    password : process.env.DB_PASSWORD,
-}
 
 var app = express();
 app.use(morgan('combined'));
-
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-  pool.query('select * from article',function(err,result){
-      if(err) {
-        res.status(500).send(err.toString()); 
-      } else{
-        res.send(JSON.stringify(result.rows));
-        }
-  });
-  
-});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/ui/article_one.html',function (req,res){
+   res.sendFile(path.join(__dirname,'ui','article_one.html')) ;
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
