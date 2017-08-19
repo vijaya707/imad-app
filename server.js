7c1d5800aa1af862c767e3fb95209a65e6ea2039
vijaya707 <707viee@gmail.com>
2017-08-19 22:53:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,9 +99,28 @@ var htmltemplate = `
 return htmltemplate;
 }
 
-app.get('/:articleName',function (req,res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName])) ;
+app.get('/articles/:articleName',function (req,res){
+    pool.query("SELECT * FROM ARTICLE WHERE TITLE="+req.params.articleName,function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+           // res.send(JSON.stringify(result.rows));
+           if(result.rows.length==0)
+            {
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData)) ;
+            }
+            
+            
+        }//else
+    });
+    
 });
 
 
