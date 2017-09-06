8fa8b92f5c5cc64d4c6b5ba0f646a55beae4f73d
vijaya707 <707viee@gmail.com>
2017-09-06 17:50:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,38 @@ imgelement.onclick = function() {
     var interval = setInterval(moveRight,50);
    
 };
+
+
+var submit = document.getElementById('submit_id');
+submit.onclick = function () {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200)
+          {
+            console.log('user logged in');
+            alert('Logged in successfully');
+              
+          }//if
+          if(request.status === 403)
+          {
+              alert('Invalid password/username');
+          }
+          if(request.status === 500)
+          {
+              alert('Something went wrong');
+          }
+      }//if
+    };
+    
+    var user = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://u707viee.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringfy({username:username,password:password}));
+};
+
